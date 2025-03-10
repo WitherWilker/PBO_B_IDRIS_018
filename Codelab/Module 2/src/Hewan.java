93e58060df0f9dba233f7410a6c383386a12e11f
WitherWilker <m.idris.saputra@gmail.com>
2025-03-10 20:46:44 +0700

    First commit

--- Codelab/Module 2/src/Hewan.java
+++ Codelab/Module 2/src/Hewan.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+public class Hewan { // Kelas Hewan
+    private String nama;
+    private String jenis;
+    private String suara;
 
-public class Hewan {
+    // Constructor
+    public Hewan(String nama, String jenis, String suara) {
+        this.nama = nama;
+        this.jenis = jenis;
+        this.suara = suara;
+    }
+
+    // Metode untuk mencetak informasi hewan
+    public void tampilkanInfo() {
+        System.out.println("Nama: " + nama);
+        System.out.println("Jenis: " + jenis);
+        System.out.println("Suara: " + suara);
+        System.out.println(); // Tambahkan baris kosong untuk pemisah
+    }
 }
